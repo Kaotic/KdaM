393f6f8c12caab7010fbef3956638934b2e9f480
Kaotic <kaotic-13@hotmail.fr>
2016-03-04 01:40:14 +0100

    semi finish URLToFile

--- src/fr/kaotic/func/httpFunction.java
+++ src/fr/kaotic/func/httpFunction.java
@@ -1,7 +1,10 @@
 package fr.kaotic.func;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
 
 public class httpFunction {
 
@@ -11,6 +14,31 @@ public class httpFunction {
 	}
 	
 	public void URLToFile(String filename, String url){
+		try{
+			BufferedInputStream in = null;
+			FileOutputStream fout = null;
+			
+			try{
+				in = new BufferedInputStream(new URL(url).openStream());
+				fout = new FileOutputStream(filename);
 		
+				byte data[] = new byte[1024];
+				int count;
+				while ((count = in.read(data, 0, 1024)) != -1)
+				{
+					fout.write(data, 0, count);
+				}
+			}finally{
+				if (in != null)
+					in.close();
+				if (fout != null)
+					fout.close();
+			}
+		}catch(MalformedURLException e1){
+			e1.printStackTrace();
+		}catch(IOException e2){
+			e2.printStackTrace();
+		}
+			
 	}
 }
